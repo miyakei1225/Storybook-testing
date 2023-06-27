792bfecc026a2a3d94290f099e4ef42426ec3f8d
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2023-06-27 10:12:06 +0900

    Add: Mock Server Worker

--- storybook-interaction-test/.storybook/preview.ts
+++ storybook-interaction-test/.storybook/preview.ts
@@ -1,15 +1,20 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react'
+import { initialize, mswLoader } from 'msw-storybook-addon'
+
+// Initialize MSW
+initialize()
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
+        date: /Date$/
+      }
+    }
   },
-};
+  loaders: [mswLoader]
+}
 
-export default preview;
+export default preview
