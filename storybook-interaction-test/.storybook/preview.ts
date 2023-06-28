ce0621f6bd2a2160fad563a1840192483ce73f2b
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2023-06-28 18:43:04 +0900

    Add: MSW mock server handler

--- storybook-interaction-test/.storybook/preview.ts
+++ storybook-interaction-test/.storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react'
 import { initialize, mswLoader } from 'msw-storybook-addon'
+import { rest } from 'msw'
 
 // Initialize MSW
 initialize()
@@ -12,6 +13,24 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/
       }
+    },
+    msw: {
+      handlers: [
+        rest.get('/api/current', async (_, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              id: 1,
+              bio: '自称寿司職人エンジニア。ヒラメと真鯛を捌くのが得意です。',
+              location: 'Tokyo, Japan',
+              name: 'Keitaro Miyano',
+              profileImageUrl: '/__mocks__/images/img01.png',
+              username: '@38ke1',
+              website: 'https://github.com/miyakei1225'
+            })
+          )
+        })
+      ]
     }
   },
   loaders: [mswLoader]
---
+++ storybook-interaction-test/src/stories/Profile.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import './test-button.css'
+
+interface ButtonProps {
+  testId: string
+  label: string
+  text?: string
+  href?: string
+  primary?: boolean
+  'data-testid'?: string
+}
+
+type Profile = {
+  id: number
+  bio: string
+  location: string
+  name: string
+  profileImageUrl: string
+  userName: string
+  website: string
+}
+
+export const Profile = ({ testId, label, text, href, primary = false, ...props }: ButtonProps) => {
+  const [currentUser, setCurrentUser] = useState<Profile>()
+
+  useEffect(() => {
+    fetch('/api/current')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data: Profile) => {
+        return setCurrentUser(data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <text>{currentUser?.id}</text>
+      <text>{currentUser?.bio}</text>
+      <text>{currentUser?.location}</text>
+      <text>{currentUser?.name}</text>
+      <text>{currentUser?.userName}</text>
+      <text>{currentUser?.website}</text>
+    </div>
+  )
+}
