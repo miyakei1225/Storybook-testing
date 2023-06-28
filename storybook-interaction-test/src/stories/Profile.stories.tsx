36a20d9809ceebac0238ac30266f43891285ba5c
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2023-06-29 04:32:17 +0900

    Add: Chromatic workflows

--- storybook-interaction-test/src/stories/Profile.stories.tsx
+++ storybook-interaction-test/src/stories/Profile.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Profile } from './Profile'
+import { within } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
 
 const meta: Meta<typeof Profile> = {
   component: Profile,
@@ -11,3 +13,17 @@ export default meta
 type Story = StoryObj<typeof Profile>
 
 export const Default: Story = {}
+
+// export const Link: Story = {
+//     args: {
+//       label: "Default",
+//       "data-testid": "button",
+//     },
+//     play: async ({ canvasElement, step }) => {
+//       const canvas = within(canvasElement);
+
+//       await step("ボタンクリック時にuserの情報がセットされる", async () => {
+//         await expect(canvas.getByText("Default")).toBeInTheDocument();
+//       });
+//     },
+//   };
